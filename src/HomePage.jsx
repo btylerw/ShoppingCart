f3e7d125a67cf1c5821d9d781cd5f60566b17d1a
Tyler <brown.tyler.w@gmail.com>
2024-09-20 14:01:07 -0700

    Push test

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const HomePage = () => {
+    // comment for git push test
+    const [shoppingCart, setShoppingCart] = useState([]);
     return (
-        <h1>This is the home page!</h1>
+        <>
+            <h1>This is the home page!</h1>
+            <div>Your Shopping Cart has: {shoppingCart.length} items</div>
+        </>
+
     )
 };
 
