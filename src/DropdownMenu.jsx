9c570ea501371fb8e9e35064755d9b311c02d1c0
Tyler <brown.tyler.w@gmail.com>
2024-09-26 08:17:21 -0700

    Fixed shop page not being full height. Categories list now functions as a proper dropdown menu.

--- src/DropdownMenu.jsx
+++ src/DropdownMenu.jsx
@@ -5,10 +5,10 @@ export const DropdownMenu = () => {
     return (
         <>
         <ul className="dropdown-menu">
-            <Link to="/shop" state={{ category: "electronics"}}>Electronics</Link>
-            <Link to="/shop" state={{ category: "jewelery" }}>Jewelry</Link>
-            <Link to="/shop" state={{ category: "men's clothing" }}>Men's Clothing</Link>
-            <Link to="/shop" state={{ category: "women's clothing" }}>Women's Clothing</Link>
+            <Link to="/shop" className="dropdown-link" state={{ category: "electronics"}}>Electronics</Link>
+            <Link to="/shop" className="dropdown-link" state={{ category: "jewelery" }}>Jewelry</Link>
+            <Link to="/shop" className="dropdown-link" state={{ category: "men's clothing" }}>Men's Clothing</Link>
+            <Link to="/shop" className="dropdown-link" state={{ category: "women's clothing" }}>Women's Clothing</Link>
         </ul>
         </>
     )
