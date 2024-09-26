afeaaaeb64903d80a1f39c3a2c62f46cdda1f0a1
Tyler <brown.tyler.w@gmail.com>
2024-09-26 08:44:54 -0700

    Navbar now ACTUALLY stays at the top

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { useState } from "react";
 import '../styles/Navbar.css'
 import shoppingCart from './assets/cart-icon.svg'
+import downArrow from './assets/expand-list.svg'
 import { DropdownMenu } from "./DropdownMenu";
 
 const Navbar = () => {
@@ -18,7 +19,7 @@ const Navbar = () => {
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px', position: 'sticky'}}>
                 <Link to="/" style={{color: 'white'}}>Home</Link>
                 <Link to="/shop" state={{category: null}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{color: 'white', position: 'relative', display: 'inline-block'}}>
-                    Shop
+                    Shop <span><img className="arrow"src={downArrow} alt="" /></span>
                     {isDropDownVisible && <DropdownMenu />}
                 </Link>
                 <Link to="/cart">
